0c314cb7d45f81b7b0a37ecec15cd1bb65997798
Garulf <535299+Garulf@users.noreply.github.com>
2024-02-25 14:18:28 -0500

    Update query.py

--- src/query.py
+++ src/query.py
@@ -1,35 +1,35 @@
-from github import Github
 from pyflowlauncher.result import ResultResponse, send_results
 from pyflowlauncher.jsonrpc import JsonRPCClient
 
+from api import Github
 from results import repo_results, scored_repo_results
 
 STARS_PREFIX = "*"
 SEPERATOR = "/"
 
 PER_PAGE = 100
-SEARCH_LIMIT = 15
+SEARCH_LIMIT = 10
 
 
-def query(query: str) -> ResultResponse:
+async def query(query: str) -> ResultResponse:
     settings = JsonRPCClient().recieve().get("settings", {})
     token = settings.get("token", None) or None
-    gh = Github(login_or_token=token, per_page=PER_PAGE)
+    gh = Github(token)
 
     repo_query = query
     parsed_query = query.split(SEPERATOR)
 
     if token:
         if query.startswith("/") or query == "":
-            repos = gh.get_user().get_repos(sort="updated")
-            return send_results(scored_repo_results(query, repos))
+            repos = gh.user_repos(sort="updated", per_page=PER_PAGE)
+            return send_results([result async for result in scored_repo_results(query, repos)])
         elif query.startswith(STARS_PREFIX):
             query = query[1:]
-            repos = gh.get_user().get_starred()
-            return send_results(scored_repo_results(query, repos))
+            repos = gh.get_starred(per_page=PER_PAGE)
+            return send_results([result async for result in scored_repo_results(query, repos)])
     if len(parsed_query) == 2:
         user, repo_query = parsed_query
-        results = repo_results(gh.search_repositories(f"user:{user} {repo_query}")[:SEARCH_LIMIT])
+        results = repo_results(gh.search_repos(f"user:{user} {repo_query}", per_page=PER_PAGE), limit=SEARCH_LIMIT)
     else:
-        results = repo_results(gh.search_repositories(repo_query)[:SEARCH_LIMIT])
-    return send_results(results)
+        results = repo_results(gh.search_repos(repo_query, per_page=PER_PAGE), limit=SEARCH_LIMIT)
+    return send_results([result async for result in results])
